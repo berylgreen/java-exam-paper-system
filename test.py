d79cc95706462def79ca43fe8470d93719893ab8
cc <cc@example.com>
2026-06-22 22:26:10 +0800

    refactor: remove auxiliary maintenance scripts and update questions metadata

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-content = open("/home/cc/server/java-exam-paper-system/project-answer/bank-encapsulation-answer/README.md").read()
-print("Total length:", len(content))
-print("Index 293 context:", repr(content[280:310]))
-print("Index 456 context:", repr(content[440:480]))
